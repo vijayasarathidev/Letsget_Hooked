56a41ec2fd16915604bb1748d6099d024630dd50
vijayasarathi <113992612+vijayasarathidev@users.noreply.github.com>
2023-07-16 15:40:45 +0530

    "useEffect"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 //import Counter from './Components/UseState/Usestate';
-import State from './Components/UseEffect/Useeffect';
+//import State from './Components/UseEffect/Useeffect';
 
 function App() {
   return (
     <div className="App">
-     <State/>
+     
     </div>
   );
 }
